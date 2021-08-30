9fb810a546f442373cc0f7beb0b864a7f063fecf
Ambika-cyber <ambikasadh12@gmail.com>
2021-08-30 21:41:30 +0530

    First commit

---
+++ corona/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+# Create your views here.
+def  homepage(request):
+    return render(request,'homepage.html')
+
+def  about(request):
+    return render(request,'about.html')
+
+
+
+
+def index(request):
+    data = True
+    result = None
+    while(data):
+        try:
+            result = requests.get('https://api.covid19api.com/summary') # get summary data from api
+            json = result.json()
+            result = json['Global']
+            countries = json['Countries']
+            data = False
+        except:
+            data = True
+
+    return render(request,'index.html', {'result':result,'country':countries})
